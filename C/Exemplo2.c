c59f3f36a5b3986e20471a3cb5ee97496bee4504
ananaminhabanana-wq <ananaminhabanana@gmail.com>
2025-09-15 08:02:26 -0300

    Create Exemplo2.c

---
+++ C/Exemplo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int a=0, b=0;
+printf("Digite um numero:");
+scanf("%d", &a);
+printf("Digite segundo numero:");
+scanf("%d", &b);
+ printf("Soma: %d\n", a+b);
+ printf("Subtraçao: %d\n", a-b);
+ printf("Divisao: %d\n", a/b);
+ printf("Multiplicaçao: %d\n", a*b);
+    return 0;
+}
